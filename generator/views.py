f18fdea1dba131b391ec7f87356ff2b27c08696e
Errol Flynn <efpdfexpert@gmail.co.uk>
2020-03-23 07:01:31 +0000

    my first commit

---
+++ generator/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'generator/home.html', {'password': 'dsafasdfe34353'})
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+def password(request):
+    characters = list("abcdefghijklmnopqrstuvwxyz")
+    password = ""
+    length = int(request.GET.get('length'))
+    for char in range(length):
+        password += random.choice(characters)
+    return render(request, 'generator/password.html', {"password": password})
